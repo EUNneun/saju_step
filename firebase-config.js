faad51cede76a870e1d04986ae5b44e277fa6599
EUNneun <101326692+EUNneun@users.noreply.github.com>
2026-09-15 14:11:24 +0900

    Add Firebase config for Google sign-in

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.SAJU_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDiVxO601t2xVkFZ0hDEZdnbM6TSg9ce1U",
+  authDomain: "tarotstep-ca085.firebaseapp.com",
+  projectId: "tarotstep-ca085",
+  storageBucket: "tarotstep-ca085.firebasestorage.app",
+  messagingSenderId: "334384256709",
+  appId: "1:334384256709:web:4b3e39c729bc3f681e82ac",
+  measurementId: "G-1EVKFJGYQ4"
+};
